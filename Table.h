9dc11f26794ff66f792090acbe585777281a001d
GozdeUnver <56361766+GozdeUnver@users.noreply.github.com>
2020-08-08 15:15:09 +0300

    Create Table.h

---
+++ Table.h
@@ -0,0 +1,22 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Table {
+private:
+	bool occupied;
+
+public:
+	Table();
+
+	void occupy();
+	void empty();
+	bool isOccupied() const;
+
+	friend ostream &operator<<(ostream &out, const Table &table);
+};
+
+#endif
